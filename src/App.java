eceaabc314c50cae169c897b047f1d3f9a9b0d30
silverThunder09 <jeg9017@gmail.com>
2026-03-19 17:43:30 +0900

    [refactor]: Main 클래스명 App으로 변경

--- src/App.java
+++ src/App.java
@@ -1,7 +1,7 @@
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class Main {
+public class App {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
