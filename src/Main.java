84a46336e9c510cc616ff1988c561817872bae3c
silverThunder09 <jeg9017@gmail.com>
2026-03-19 11:30:48 +0900

    [feat]: 계산기 사칙연산 기능 추가

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,51 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
 
+        while (true) {
+            System.out.print("첫 번째 숫자를 입력하세요: ");
+
+            int num1 = sc.nextInt();
+
+            System.out.print("두 번째 숫자를 입력하세요: ");
+
+            int num2 = sc.nextInt();
+
+            System.out.print("사칙연산 기호를 입력하세요: ");
+
+            char operator = sc.next().charAt(0);
+
+            int result = 0;
+            boolean isValid = true;
+
+            switch (operator) {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    result = num1 / num2;
+                    break;
+
+                default:
+                    System.out.println("잘못된 사칙연산 기호 입니다.");
+                    isValid = false;
+            }
+
+            if (isValid) {
+                System.out.println("결과: " + result);
+                break;
+            }
+
+        }
+        sc.close();
     }
 }
