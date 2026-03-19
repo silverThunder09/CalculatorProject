9f89bd41766bbfaa0eaf92ba3b88f35c0456cffe
silverThunder09 <jeg9017@gmail.com>
2026-03-19 11:51:46 +0900

    [fix]: 나눗셈 연산중 두번째 정수가 0이면 오류 메시지 출력 후 재입력 처리

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -6,45 +7,54 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         while (true) {
-            System.out.print("첫 번째 숫자를 입력하세요: ");
+            try{
+                System.out.print("첫 번째 숫자를 입력하세요: ");
 
-            int num1 = sc.nextInt();
+                int num1 = sc.nextInt();
 
-            System.out.print("두 번째 숫자를 입력하세요: ");
+                System.out.print("두 번째 숫자를 입력하세요: ");
 
-            int num2 = sc.nextInt();
+                int num2 = sc.nextInt();
 
-            System.out.print("사칙연산 기호를 입력하세요: ");
+                System.out.print("사칙연산 기호를 입력하세요: ");
 
-            char operator = sc.next().charAt(0);
+                char operator = sc.next().charAt(0);
 
-            int result = 0;
-            boolean isValid = true;
+                int result = 0;
+                boolean isValid = true;
 
-            switch (operator) {
-                case '+':
-                    result = num1 + num2;
+                switch (operator) {
+                    case '+':
+                        result = num1 + num2;
+                        break;
+                    case '-':
+                        result = num1 - num2;
+                        break;
+                    case '*':
+                        result = num1 * num2;
+                        break;
+                    case '/':
+                        if(num2 == 0){
+                            System.out.println("나눗셈 연산에서 두번째 정수에 0이 입력될 수 없습니다.");
+                            isValid = false;
+                        } else {
+                            result = num1 / num2;
+                        }
+                        break;
+
+                    default:
+                        System.out.println("잘못된 사칙연산 기호 입니다.");
+                        isValid = false;
+                }
+
+                if (isValid) {
+                    System.out.println("결과: " + result);
                     break;
-                case '-':
-                    result = num1 - num2;
-                    break;
-                case '*':
-                    result = num1 * num2;
-                    break;
-                case '/':
-                    result = num1 / num2;
-                    break;
-
-                default:
-                    System.out.println("잘못된 사칙연산 기호 입니다.");
-                    isValid = false;
+                }
+            } catch (InputMismatchException e){
+                System.out.println("숫자를 입력해주세요.");
+                sc.nextLine();
             }
-
-            if (isValid) {
-                System.out.println("결과: " + result);
-                break;
-            }
-
         }
         sc.close();
     }
