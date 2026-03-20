f893282f8275db31fab5f7f46b580d8dcaebadd4
silverThunder09 <jeg9017@gmail.com>
2026-03-21 03:55:07 +0900

    [feat]: Calculator 클래스 생성 및 calculate 메서드 구현

---
+++ src/Calculator.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Calculator {
+
+    List<Integer> results = new ArrayList<>();
+
+    public int calculate(int num1, int num2, char operator) {
+        int result = 0;
+        boolean isValid = true;
+
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+
+            case '-':
+                result = num1 - num2;
+                break;
+
+            case '*':
+                result = num1 * num2;
+                break;
+
+            case '/':
+                if(num2 == 0) {
+                    System.out.println("나눗셈 연산에서 두번째 정수에 0이 입력될 수 없습니다.");
+                    isValid = false;
+                } else {
+                    result = num1 /num2;
+                }
+                break;
+            default:
+                System.out.println("잘못된 사칙연산 기호 입니다.");
+                isValid = false;
+
+        }
+
+
+        if (isValid) {
+            results.add(result);
+        }
+
+        return result;
+    }
+}
+
